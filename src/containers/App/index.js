e2956d1a3fb43f1e483789cce0efdb8fc6964f6e
Nurguly Ashyrov <nurguly.ashyrov@gmail.com>
2020-02-11 16:56:28 +0400

    Create a Rooms container.

--- src/containers/App/index.js
+++ src/containers/App/index.js
@@ -1,18 +1,9 @@
 import React from "react";
 
-import Stripe from "components/Stripe";
-import colors from "styles/colors";
-import styles from "./App.module.scss";
+import Rooms from "containers/Rooms";
 
 function App() {
-  return (
-    <Stripe
-      collection="fast-track"
-      name="south_sea_island_fantasy"
-      color={colors.red}
-      className={styles.stripe}
-    />
-  );
+  return <Rooms />;
 }
 
 export default App;
---
+++ src/containers/Rooms/index.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Rooms() {
+  return <h1>Rooms</h1>;
+}
+
+export default Rooms;
