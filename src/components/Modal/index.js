4b928fa358b75a94d8dc532f0cb31d6b00902f17
Nurguly Ashyrov <nurguly.ashyrov@gmail.com>
2020-02-17 13:07:00 +0400

    Animate Modal component.

--- src/components/Modal/index.js
+++ src/components/Modal/index.js
@@ -3,6 +3,8 @@ import ReactModal from "react-modal";
 
 import styles from "./styles.module.scss";
 
+export const TRANSITION_TIME = 120;
+
 const Modal = props => (
   <ReactModal
     className={{
@@ -10,7 +12,7 @@ const Modal = props => (
       afterOpen: styles.afterOpen,
       beforeClose: styles.beforeClose
     }}
-    closeTimeoutMS={400}
+    closeTimeoutMS={TRANSITION_TIME}
     {...props}
   />
 );
--- src/containers/Rooms/SelectRoom/index.js
+++ src/containers/Rooms/SelectRoom/index.js
@@ -1,19 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 import { useSelector } from "react-redux";
 
-import Modal from "src/components/Modal";
+import Modal, { TRANSITION_TIME } from "src/components/Modal";
 import { selectRoom } from "src/containers/Rooms/selectors";
 
 const SelectRoom = ({ roomId, navigate }) => {
+  const [isOpen, setIsOpen] = useState(true);
   const room = useSelector(selectRoom(roomId));
   if (!room) {
     return null;
   }
+
+  function handleRequestClose() {
+    setIsOpen(false);
+    setTimeout(() => navigate("/rooms"), TRANSITION_TIME);
+  }
   return (
-    <Modal
-      isOpen={!!room && !!room.timeCreated}
-      onRequestClose={() => navigate("/rooms")}
-    >
+    <Modal isOpen={isOpen} onRequestClose={handleRequestClose}>
       <span>{room.name}</span>
     </Modal>
   );
