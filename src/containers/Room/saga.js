3f2f602f9d55747b6cf7f4ea714772a26055d3f3
Nurguly Ashyrov <nurguly.ashyrov@gmail.com>
2020-02-18 19:56:59 +0400

    Do not fetch the room data if it is already there.

--- src/containers/Room/saga.js
+++ src/containers/Room/saga.js
@@ -1,10 +1,14 @@
-import { takeLatest, call } from "redux-saga/effects";
+import { takeLatest, call, select } from "redux-saga/effects";
 
 import { db } from "src/services/Firebase";
 import { createUpdaterSaga } from "src/redux/sagaHelpers";
 import { joinRoomStart, joinRoomSuccess, joinRoomError } from "./reducer";
 
 function* joinRoomStartSaga({ payload: { roomId } }) {
+  const alreadyJoined = yield select(s => s.room.id === roomId);
+  if (alreadyJoined) {
+    return;
+  }
   yield call(createUpdaterSaga, {
     dbRef: db.ref(`/rooms/${roomId}`),
     normalizeData: ([id, data]) => ({ id, data: { id, ...data } }),
