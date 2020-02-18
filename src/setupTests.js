fa420914cc67037c76705bdd05e0a009fa5aa7f3
Nurguly Ashyrov <nurguly.ashyrov@gmail.com>
2020-02-18 16:22:56 +0400

    Add canvas support for testing environment.

--- src/setupTests.js
+++ src/setupTests.js
@@ -3,3 +3,7 @@
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import "@testing-library/jest-dom/extend-expect";
+
+import * as canvas from "canvas";
+
+window.CanvasRenderingContext2D = canvas.CanvasRenderingContext2D;
