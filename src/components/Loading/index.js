12c0f4fba4749bb5debd0f911634cdb6757bbd91
Nurguly Ashyrov <nurguly.ashyrov@gmail.com>
2020-02-18 20:24:30 +0400

    Create a Loading component.

---
+++ src/components/Loading/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+import Container from "src/components/Container";
+
+const Loading = () => (
+  <Container>
+    <h3>Loading</h3>
+  </Container>
+);
+
+export default Loading;
