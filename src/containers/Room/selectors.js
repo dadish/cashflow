a2264b9211e8716f1c420925a192e9b274846e38
Nurguly Ashyrov <nurguly.ashyrov@gmail.com>
2020-02-18 19:07:07 +0400

    Select extended room state if available at selectRoom.

--- src/containers/Room/selectors.js
+++ src/containers/Room/selectors.js
@@ -3,5 +3,11 @@ import { createSelector } from "@reduxjs/toolkit";
 export const selectRoom = id =>
   createSelector(
     state => state.rooms.data,
-    rooms => rooms.find(room => room.id === id)
+    state => state.room,
+    (rooms, room) => {
+      if (room.id === id) {
+        return room;
+      }
+      return rooms.find(room => room.id === id);
+    }
   );
