8a94b7c30ad1682e4c1d2bfbb6060c2184034023
Nurguly Ashyrov <nurguly.ashyrov@gmail.com>
2020-02-18 14:46:17 +0400

    Enable old Stripe reducer tests.

--- src/components/Stripe/reducer.test.js
+++ src/components/Stripe/reducer.test.js
@@ -9,12 +9,12 @@ import reducer, {
 
 const collection = "fast-track";
 
-test.skip("imageLoadInit action changes nothing", () => {
+test("imageLoadInit action changes nothing", () => {
   const nextState = reducer(initialState, imageLoadInit({ collection }));
   expect(nextState).toBe(initialState);
 });
 
-test.skip("imageLoadStart action sets the collection inProgress to true and clear the error if any", () => {
+test("imageLoadStart action sets the collection inProgress to true and clear the error if any", () => {
   const state = createNextState(initialState, draft => {
     draft.images[collection].error = new Error("Some error");
   });
@@ -24,7 +24,7 @@ test.skip("imageLoadStart action sets the collection inProgress to true and clea
   expect(nextState.images[collection].error).toBe(null);
 });
 
-test.skip("imageLoadStart action does nothing if no collection match found", () => {
+test("imageLoadStart action does nothing if no collection match found", () => {
   const nextState = reducer(
     initialState,
     imageLoadStart({ collection: "some-random-thing" })
@@ -32,11 +32,12 @@ test.skip("imageLoadStart action does nothing if no collection match found", () 
   expect(nextState).toBe(initialState);
 });
 
-test.skip("imageLoadSuccess action sets the collection inProgress to false and sets the canvasContext", () => {
+test("imageLoadSuccess action sets the collection inProgress to false and sets the canvasContext", () => {
   const state = createNextState(initialState, draft => {
     draft.images[collection].inProgress = true;
   });
-  const canvasContext = new CanvasRenderingContext2D();
+  const canvas = document.createElement("canvas");
+  const canvasContext = canvas.getContext("2d");
   const nextState = reducer(
     state,
     imageLoadSuccess({ collection, canvasContext })
@@ -46,7 +47,7 @@ test.skip("imageLoadSuccess action sets the collection inProgress to false and s
   expect(nextState.images[collection].canvasContext).toBe(canvasContext);
 });
 
-test.skip("imageLoadSuccess action does nothing if no collection match found", () => {
+test("imageLoadSuccess action does nothing if no collection match found", () => {
   const nextState = reducer(
     initialState,
     imageLoadSuccess({ collection: "some-random-thing" })
@@ -54,7 +55,7 @@ test.skip("imageLoadSuccess action does nothing if no collection match found", (
   expect(nextState).toBe(initialState);
 });
 
-test.skip("imageLoadError action sets the collection inProgress to false and sets the error", () => {
+test("imageLoadError action sets the collection inProgress to false and sets the error", () => {
   const state = createNextState(initialState, draft => {
     draft.images[collection].inProgress = true;
   });
@@ -65,7 +66,7 @@ test.skip("imageLoadError action sets the collection inProgress to false and set
   expect(nextState.images[collection].error).toBe(error);
 });
 
-test.skip("imageLoadError action does nothing if no collection match found", () => {
+test("imageLoadError action does nothing if no collection match found", () => {
   const nextState = reducer(
     initialState,
     imageLoadError({ collection: "some-random-thing" })
