bb8fe35ba11290cc3adfc11caabfdc54b9918f45
Nurguly Ashyrov <nurguly.ashyrov@gmail.com>
2020-02-18 15:33:48 +0400

    Remove canvas mocks from testing environment.

--- src/setupTests.js
+++ src/setupTests.js
@@ -3,7 +3,3 @@
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import "@testing-library/jest-dom/extend-expect";
-
-import { CanvasRenderingContext2D } from "canvas";
-
-window.CanvasRenderingContext2D = CanvasRenderingContext2D;
