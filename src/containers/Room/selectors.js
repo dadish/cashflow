bf932d992ed1559978e6f6ccd9f3f59726f0a342
Nurguly Ashyrov <nurguly.ashyrov@gmail.com>
2020-02-19 16:59:49 +0400

    Create selectNumPlayers selector.

--- src/containers/Room/selectors.js
+++ src/containers/Room/selectors.js
@@ -11,3 +11,12 @@ export const selectRoom = id =>
       return rooms.find(room => room.id === id);
     }
   );
+
+export const selectNumPlayers = id =>
+  createSelector(selectRoom(id), room => {
+    let numPlayers = 0;
+    if (room && room.players && room.players.length) {
+      numPlayers = room.players.filter(({ userId }) => userId).length;
+    }
+    return numPlayers;
+  });
